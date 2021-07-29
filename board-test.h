7cf39b740dd9239acd5b896ded408a6fc6f68871
IvyJiang7 <jiangyuetongtong@yeah.net>
2021-07-29 21:04:41 +0800

    added all methods

--- board-test.h
+++ board-test.h
@@ -4,6 +4,7 @@
 #include "vertex.h"
 #include "edge.h"
 #include <vector>
+#include <string>
 
 class Board {
     std::vector<Tile*> tiles;
@@ -11,7 +12,33 @@ class Board {
     std::vector<Edge*> edges;
 
     public:
-    void initAttachBoard();
+    // ctor
+    Board();
+    void clearBoard();
     void printBoard();
+    void printAllPlayerStatus();
+    void printCurPlayerStatus();
+
+    void initAttachBoard();
+    void initRandBoard();
+    void initLoadBoard( std::string file );
+    void initSeedBoard( int seed );
+    void loadGame( std::string file );
+    std::string saveGame();
+
+    void buildRes( int pos );
+    void buildRoad( int pos );
+    void improveRes( int pos );
+
+    void trade( char otherplayer, char ownResources, char otherResource );
+    void moveGeese( int pos );
+    void endCurTurn();
+
+
+
+
+    
+    
+
 
 };
