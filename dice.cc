e29f5fa1c01b197a95e56b2005c3e67a06794edd
ShuchenLiu666 <64973420+ShuchenLiu666@users.noreply.github.com>
2021-07-30 02:28:48 +0800

    Create dice.cc

---
+++ dice.cc
@@ -0,0 +1,16 @@
+#include "dice.h"
+
+Dice::Dice(Strategy* strategy, int dicePoint, int seed)
+    : strategy{strategy}, dicePoint{dicePoint}, seed{seed} {}
+
+void Dice::setStrategy(Strategy* strategy) {
+        this->strategy = strategy;
+}
+
+void Dice::roll() {
+    strategy->rollDice(dicePoint, seed);
+}
+
+int Dice::getPoint() {
+    return dicePoint;
+}
