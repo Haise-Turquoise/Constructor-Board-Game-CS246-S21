5286c3fe1aaeb7a18e9c38d7fca66b157ad7fc62
ShuchenLiu666 <64973420+ShuchenLiu666@users.noreply.github.com>
2021-07-30 02:27:22 +0800

    Create dice.h

---
+++ dice.h
@@ -0,0 +1,23 @@
+#ifndef __DICE_H__
+#define __DICE_H__
+#include "strategy.h"
+
+
+// use of Dice:
+// Dice dice = Dice{new DiceLoad, pointWeWant};   --- roll loaded dice
+// Dice dice = Dice{new DiceRand, 0 (placeholder), seed};       --- roll random dice
+// strategy should be wrapped insmart pointer
+
+
+class Dice {
+    Strategy* strategy;
+    int dicePoint;
+    int seed;
+    public:
+    Dice(Strategy* strategy, int dicePoint = 0, int seed = 0);
+    void setStrategy(Strategy* strategy);
+    void roll();
+    int getPoint();
+};
+
+#endif
