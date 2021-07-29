3a3c385a1e84a834c9177609e70ab9cdc733e7e7
Haise-Turquoise <68551627+Haise-Turquoise@users.noreply.github.com>
2021-07-29 17:22:25 +0800

    Update edge-untest.cc
    update nullptr check to avoid seg fault and update doubly attach method

--- edge-untest.cc
+++ edge-untest.cc
@@ -1,4 +1,7 @@
 #include "edge.h"
+#include "vertex.h"
+#include <iostream>
+using namespace std;
 
 Edge::Edge(int idx): index{idx}, ownerPos{-1} {}
 Edge::~Edge() {}
@@ -9,10 +12,18 @@ int Edge::getOwnerPos() { return ownerPos; }
 void Edge::setIndex(int idx) { index = idx; }
 void Edge::setOwnerPos(int op) { ownerPos = op; }
 
-void Vertex::attachVertex( Vertex* ptrv ) {
+void Edge::attachVertex( Vertex* ptrv ) {
+    if ( ptrv == nullptr ) cout << "WARNING: NULLPTR parameter in Edge::attachVertex!" << endl;
     neighbourVertices.emplace_back(ptrv);
 }
 
-void Vertex::attachEdge( Edge* ptre ) {
+void Edge::attachEdge( Edge* ptre ) {
+    if ( ptre == nullptr ) cout << "WARNING: NULLPTR parameter in Edge::attachEdge!" << endl;
     neighbourEdges.emplace_back(ptre);
+}
+
+void Edge::attachVertexDoubly( Vertex* ptrv ) {
+    if ( ptrv == nullptr ) cout << "WARNING: NULLPTR parameter in Edge::attachVertexDoubly!" << endl;
+    neighbourVertices.emplace_back(ptrv);
+    ptrv->attachEdge(this);
 }
