6fb0ef59461c01706842fa8940445d9812806aba
IvyJiang7 <jiangyuetongtong@yeah.net>
2021-07-30 16:53:11 +0800

    a wrapper for main

---
+++ ctorGame.h
@@ -0,0 +1,32 @@
+#ifndef _CTORGAME_H_
+#define _CTORGAME_H_
+#include <string>
+
+class Board;
+
+class CtorGame {
+    std::string file;
+    int seed;
+    bool load;
+    bool boardGiven;
+    bool randBoard;
+
+    public:
+    CtorGame(std::string file = "", int seed = -1, bool load = 0, bool boardGiven = 0, bool randBoard = 0);
+    void setFile(std::string file);
+    void setSeed(int seed);
+    void setLoad(bool load);
+    void setBoard(bool boardGiven);
+    void setRandBoard(bool randBoard);
+    
+    
+    int setUp(Board & board, const vector<string> fourPlayers); // return -1 when get EOF
+    void endGame(Board & board, string fileName = "backup.sv");
+    bool play();
+    
+    // for debug
+    void info();
+};
+ 
+
+#endif
