c0ed96a546fe31b6c1b464893b0e0fc3abdd7a8c
IvyJiang7 <jiangyuetongtong@yeah.net>
2021-07-30 16:52:24 +0800

    add a few more methods

--- board.h
+++ board.h
@@ -3,17 +3,27 @@
 #include "tile.h"
 #include "vertex.h"
 #include "edge.h"
+#include "player.h"
 #include <vector>
 #include <string>
+#include <queue>
 
 class Board {
+    int posGeese = 7 ;
+    int seed = 0;
     std::vector<Tile*> tiles;
     std::vector<Vertex*> vertices;
     std::vector<Edge*> edges;
+    //std::vector<Player*> players;
+    //std::queue<char> playerQueue;
+    int curTurn = 0;
 
     public:
     // ctor
     Board();
+    int getCurTurn() const;
+    void setCurTurn(int ind);
+
     void clearBoard();
     void printBoard();
     void printAllPlayerStatus();
@@ -26,17 +36,18 @@ class Board {
     void loadGame( std::string file );
     std::string saveGame();
 
+    void rollDice(bool fair);
+
     void buildRes( int pos );
     void buildRoad( int pos );
     void improveRes( int pos );
 
-    void trade( char otherplayer, char ownResources, char otherResource );
+    void trade( std::string otherplayer, std::string ownResources, std::string otherResource );
     void moveGeese( int pos );
+
+    bool checkWon();
+
     void endCurTurn();
-
-
-
-
     
     
 
