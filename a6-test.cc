fbbd28861dc820849537b19c6060f169e2cc8909
Haise-Turquoise <68551627+Haise-Turquoise@users.noreply.github.com>
2021-07-31 19:24:12 +0800

    Create a6-test.cc

---
+++ a6-test.cc
@@ -0,0 +1,16 @@
+#include "board.h"
+#include "tile.h"
+#include "vertex.h"
+#include "edge.h"
+#include "subject.h"
+#include "observer.h"
+#include "player.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    Board b;
+    b.initSeedBoard(231);
+    b.printAllPlayerStatus();
+    b.printCurPlayerRes();
+}
