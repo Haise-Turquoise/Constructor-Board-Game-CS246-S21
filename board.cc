97aa9ef39ab5fe9d752e6bee2c28675c181a6291
Haise-Turquoise <68551627+Haise-Turquoise@users.noreply.github.com>
2021-07-31 11:38:24 +0800

    Update board.cc

--- board.cc
+++ board.cc
@@ -316,7 +316,13 @@ void Board::rollDice(bool fair) {
 
 void Board::gainResources(int tileVal){
     // notify observers of tileVal
-    // only notify which has been build 
+    // only notify which has been build
+    for ( size_t i = 0; i < tiles.size(); i++ ) {
+        if ( tiles[i] == nullptr ) { cout << "WARNING: Board::gainResources dereference nullptr at line:" << __LINE__ << endl; return false; }
+        if ( tiles[i]->getValue() == tileVal ) {
+            tiles[i]->notifyObservers();
+        }
+    }
 }
 
 bool Board::buildResFree( int pos ) {
