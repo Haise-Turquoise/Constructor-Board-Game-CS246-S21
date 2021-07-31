6930da65215f6df9a11a3eeab5616a7b8249368d
Haise-Turquoise <68551627+Haise-Turquoise@users.noreply.github.com>
2021-07-31 10:47:17 +0800

    Update edge.cc

--- edge.cc
+++ edge.cc
@@ -1,16 +1,21 @@
 #include "edge.h"
 #include "vertex.h"
+#include "player.h"
 #include <iostream>
 using namespace std;
 
-Edge::Edge(int idx): index{idx}, ownerPos{-1} {}
+Edge::Edge(int idx): index{idx}, ownerPos{-1}, owner{nullptr} {}
 Edge::~Edge() {}
 
 int Edge::getIndex() { return index; }
 int Edge::getOwnerPos() { return ownerPos; }
+Player* Edge::getOwner() { return owner; }
+vector<Vertex*> Edge::getNeighbourVertices() { return neighbourVertices; }
+
 
 void Edge::setIndex(int idx) { index = idx; }
 void Edge::setOwnerPos(int op) { ownerPos = op; }
+void Edge::setOwner(Player* ow) { owner = ow; }
 
 void Edge::attachVertex( Vertex* ptrv ) {
     if ( ptrv == nullptr ) cout << "WARNING: NULLPTR parameter in Edge::attachVertex!" << endl;
