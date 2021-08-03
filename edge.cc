b4a7148614293fa4dbad0c0f31d4ab4c9cfe68ef
IvyJiang7 <jiangyuetongtong@yeah.net>
2021-08-03 16:21:07 +0800

    smart ptr

--- edge.cc
+++ edge.cc
@@ -10,18 +10,19 @@ Edge::~Edge() {}
 int Edge::getIndex() { return index; }
 int Edge::getOwnerPos() { return ownerPos; }
 Player* Edge::getOwner() { return owner; }
-vector<Vertex*> Edge::getNeighbourVertices() { return neighbourVertices; }
+vector<share_ptr<Vertex>> Edge::getNeighbourVertices() { return neighbourVertices; }
 
 
 void Edge::setIndex(int idx) { index = idx; }
 void Edge::setOwnerPos(int op) { ownerPos = op; }
 void Edge::setOwner(Player* ow) { owner = ow; }
 
-void Edge::attachVertex( Vertex* ptrv ) {
+void Edge::attachVertex( shared_ptr<Vertex> ptrv ) {
     if ( ptrv == nullptr ) cout << "WARNING: NULLPTR parameter in Edge::attachVertex!" << endl;
     neighbourVertices.emplace_back(ptrv);
 }
 
+/*
 void Edge::attachEdge( Edge* ptre ) {
     if ( ptre == nullptr ) cout << "WARNING: NULLPTR parameter in Edge::attachEdge!" << endl;
     neighbourEdges.emplace_back(ptre);
@@ -32,3 +33,4 @@ void Edge::attachVertexDoubly( Vertex* ptrv ) {
     neighbourVertices.emplace_back(ptrv);
     ptrv->attachEdge(this);
 }
+*/
