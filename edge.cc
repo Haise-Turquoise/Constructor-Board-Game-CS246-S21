00e28c9d6b6611c411c65e2641a3eff979e30bc5
IvyJiang7 <jiangyuetongtong@yeah.net>
2021-08-03 17:56:23 +0800

    remove unused methods

--- edge.cc
+++ edge.cc
@@ -8,11 +8,10 @@ using namespace std;
 Edge::Edge(int idx): index{idx}, ownerPos{-1}, owner{nullptr} {}
 Edge::~Edge() {}
 
-int Edge::getIndex() { return index; }
-int Edge::getOwnerPos() { return ownerPos; }
-Player* Edge::getOwner() { return owner; }
-vector<Vertex*> Edge::getNeighbourVertices() { return neighbourVertices; }
-
+int Edge::getIndex() const { return index; }
+int Edge::getOwnerPos() const { return ownerPos; }
+Player* Edge::getOwner() const { return owner; }
+vector<Vertex*> Edge::getNeighbourVertices() const { return neighbourVertices; }
 
 void Edge::setIndex(int idx) { index = idx; }
 void Edge::setOwnerPos(int op) { ownerPos = op; }
@@ -22,16 +21,3 @@ void Edge::attachVertex( Vertex* ptrv ) {
     if ( ptrv == nullptr ) cout << "WARNING: NULLPTR parameter in Edge::attachVertex!" << endl;
     neighbourVertices.emplace_back(ptrv);
 }
-
-/*
-void Edge::attachEdge( Edge* ptre ) {
-    if ( ptre == nullptr ) cout << "WARNING: NULLPTR parameter in Edge::attachEdge!" << endl;
-    neighbourEdges.emplace_back(ptre);
-}
-
-void Edge::attachVertexDoubly( Vertex* ptrv ) {
-    if ( ptrv == nullptr ) cout << "WARNING: NULLPTR parameter in Edge::attachVertexDoubly!" << endl;
-    neighbourVertices.emplace_back(ptrv);
-    ptrv->attachEdge(this);
-}
-*/
