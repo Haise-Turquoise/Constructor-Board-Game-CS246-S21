829f27135a84ba007538f21f241689f7fcacfced
IvyJiang7 <jiangyuetongtong@yeah.net>
2021-08-03 16:12:27 +0800

    smart ptr

--- vertex.cc
+++ vertex.cc
@@ -3,33 +3,36 @@
 #include "edge.h"
 #include "player.h"
 #include <iostream>
+#include <memory>
 using namespace std;
 
 Vertex::Vertex(int idx): index{idx}, buildType{'-'}, ownerPos{-1}, owner{nullptr} {}
 Vertex::~Vertex() {}
 
-int Vertex::getIndex() { return index; }
-char Vertex::getBuildType() { return buildType; }
-int Vertex::getOwnerPos() { return ownerPos; }
-Player* Vertex::getOwner() { return owner; }
-vector<Edge*> Vertex::getNeighbourEdges() { return neighbourEdges; }
+int Vertex::getIndex() const { return index; }
+char Vertex::getBuildType() const { return buildType; }
+int Vertex::getOwnerPos() const { return ownerPos; }
+Player* Vertex::getOwner() const { return owner; }
+shared_ptr<Edge>> Vertex::getNeighbourEdges() const { return neighbourEdges; }
 
 void Vertex::setIndex(int idx) { index = idx; }
 void Vertex::setBuildType(char bt) { buildType = bt; }
 void Vertex::setOwnerPos(int op) { ownerPos = op; }
 void Vertex::setOwner( Player* ptrp ) { owner = ptrp; }
 
+/*
 void Vertex::attachVertex( Vertex* ptrv ) {
-    if ( ptrv == nullptr ) cout << "WARNING: NULLPTR parameter in Vertex::attachVertex!" << endl;
+    if ( ptrv == nullptr ) cout << "WARNING: NULLPTR parameter in Vertex::attachVertex!" << endl; 
     neighbourVertices.emplace_back(ptrv);
 }
+*/
 
-void Vertex::attachEdge( Edge* ptre ) {
-    if ( ptre == nullptr ) cout << "WARNING: NULLPTR parameter in Vertex::attachEdge!" << endl;
+void Vertex::attachEdge( shared_ptr<Edge> ptre ) {
+    if ( ptre == nullptr ) cout << "WARNING: NULLPTR parameter in Vertex::attachEdge!" << endl; 
     neighbourEdges.emplace_back(ptre);
 }
 
-void Vertex::attachEdgeDoubly( Edge* ptre ) {
+void Vertex::attachEdgeDoubly( shared_ptr<Edge> ptre ) {
     if ( ptre == nullptr ) cout << "WARNING: NULLPTR parameter in Vertex::attachEdgeDoubly!" << endl;
     neighbourEdges.emplace_back(ptre);
     ptre->attachVertex(this);
