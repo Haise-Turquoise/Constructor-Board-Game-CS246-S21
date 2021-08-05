effa0868fbe2b0de7c8700cef2f73a439e96066c
Haise-Turquoise <68551627+Haise-Turquoise@users.noreply.github.com>
2021-08-05 11:18:54 +0800

    Update vertex.cc

--- vertex.cc
+++ vertex.cc
@@ -29,6 +29,8 @@ void Vertex::attachEdgeDoubly( Edge* ptre ) {
 void Vertex::wasNotified( Tile& whoNotified ) {
     if ( owner != nullptr ) {
         char tileResourceType = whoNotified.getResourceType();
-        owner->addResource(tileResourceType);
+        if ( buildType == 'B' || buildType == 'H' || buildType == 'T') { owner->addResource(tileResourceType); }
+        if ( buildType == 'H' || buildType == 'T') { owner->addResource(tileResourceType); }
+        if ( buildType == 'T') { owner->addResource(tileResourceType); }
     }
 }
